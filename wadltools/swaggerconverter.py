cc0026ae0b4a22c4d566a1c117dd9005d32ead8a
unclemusclez <unclemusclez@gmail.com>
2025-10-24 10:08:39 -0400

    remove spaced formatting.

--- wadltools/swaggerconverter.py
+++ wadltools/swaggerconverter.py
@@ -7,8 +7,7 @@ import logging
 from collections import OrderedDict
 import wadllib
 from wadltools.wadl import WADL, DocHelper, BadWADLError
-from wadllib.application import WADLError, UnsupportedMediaTypeError
-import wadllib.application
+from wadllib.application import Parameter, WADLError, UnsupportedMediaTypeError
 
 
 class WADLParseError(Exception):
@@ -49,6 +48,140 @@ class SwaggerConverter:
         self.autofix = options.autofix
         self.strict = options.strict
         self.merge_dir = options.merge_dir
+        self.wadl_file = None
+
+    def _process_resource_parameters(self, resource, path, swagger_resource):
+        """Process parameters for a resource to reduce nesting."""
+        try:
+            # wadllib can't get parameters w/out media types (e.g. path params?)
+            try:
+                params = resource.parameters("application/json")
+            except (UnsupportedMediaTypeError, AttributeError):
+                self.logger.warning(
+                    "No support for application/json for resource at %s", path
+                )
+                params = []
+            for param in resource.tag.findall("./" + WADL.qname("wadl", "param")):
+                params.append(Parameter(resource, param))
+            for param in params:
+                swagger_param = self.build_param(param)
+                if swagger_param is not None:
+                    if "parameters" not in swagger_resource:
+                        swagger_resource["parameters"] = []
+                    swagger_resource["parameters"].append(swagger_param)
+        except AttributeError:
+            self.logger.debug(
+                "   WARN: wadllib can't get parameters, possibly a wadllib bug"
+            )
+            self.logger.debug(
+                "     (It seems like it only works if the resource has a GET method"
+            )
+
+    def _process_method(self, method, resource, path, swagger_resource):
+        """Process a method to reduce nesting."""
+        self.logger.debug("    Processing method %s %s", method.name, path)
+        verb = method.name
+        if self.autofix and verb == "copy":
+            self.logger.warning(
+                "Autofix: Using PUT instead of COPY verb (OpenStack services accept either, Swagger does not allow COPY)"
+            )
+            verb = "put"
+        swagger_method = swagger_resource[verb] = OrderedDict()
+        # Rackspace specific...
+        if "{http://docs.rackspace.com/api}id" in method.tag.attrib:
+            swagger_method["operationId"] = method.tag.attrib[
+                "{http://docs.rackspace.com/api}id"
+            ]
+        swagger_method["summary"] = self.build_summary(method, path)
+        description = DocHelper.short_desc_as_markdown(method)
+        if description is not None:
+            swagger_method["description"] = folded(description)
+        if "id" in method.tag.attrib:
+            swagger_method["operationId"] = method.tag.attrib["id"]
+        # swagger_method['consumes'] = []
+        swagger_method["produces"] = []
+        swagger_method["responses"] = OrderedDict()
+
+        # Handle request
+        if method.request.tag is not None:
+            request = method.request
+            for representation in request.representations:
+                # Swagger schema needs to be updated to allow consumes here
+                # swagger_method['consumes'].append(representation.media_type)
+                for param in representation.params(resource):
+                    swagger_param = self.build_param(param)
+                    if swagger_param is not None:
+                        if "parameters" not in swagger_method:
+                            swagger_method["parameters"] = []
+                        swagger_method["parameters"].append(swagger_param)
+            # Add params from direct <param> tags in request
+            for param in request.tag.findall("./" + WADL.qname("wadl", "param")):
+                param_obj = Parameter(request, param)
+                swagger_param = self.build_param(param_obj)
+                if swagger_param is not None:
+                    if "parameters" not in swagger_method:
+                        swagger_method["parameters"] = []
+                    swagger_method["parameters"].append(swagger_param)
+
+        # Handle response
+        self._process_response(method, swagger_method)
+
+    def _process_response(self, method, swagger_method):
+        """Process response and handle status autofix."""
+        if method.response.tag is not None:
+            response = method.response
+            # Not properly iterable - plus we're focused on json
+            # for representation in response.representation:
+            representation = response.get_representation_definition("application/json")
+            if representation is not None:
+                swagger_method["produces"].append(representation.media_type)
+
+            # Handle status
+            try:
+                statuses = response.tag.attrib["status"].split()
+            except KeyError as e:
+                if self.autofix:
+                    self.logger.warning(
+                        "Autofix: No status in response, setting default to '200'"
+                    )
+                    statuses = ["200"]
+                else:
+                    raise BadWADLError(
+                        "Response has no status", e, self.wadl_file
+                    ) from e
+
+            for status in statuses:
+                swagger_method["responses"][status] = self.build_response(
+                    response, status
+                )
+
+                code_sample = None
+                code_samples = response.tag.findall(
+                    ".//"
+                    + WADL.qname("docbook", "programlisting")
+                    + '[@language="javascript"]'
+                )
+                if code_samples:
+                    try:
+                        # if there is more than one, the first is usually HTTP headers
+                        code_sample = code_samples[-1].text
+                        code_sample = self.fix_json(code_sample)
+                    except ValueError as e:
+                        error = WADLParseError(
+                            "Unparsable code sample",
+                            self.wadl_file,
+                            swagger_method["summary"],
+                            e,
+                        )
+                        if self.strict:
+                            raise error
+                        else:
+                            self.logger.error(str(error))
+
+                if code_sample:
+                    swagger_method["responses"][status]["examples"] = (
+                        self.build_code_sample(code_sample)
+                    )
 
     def convert(self, title, wadl_file, swagger_file):
         try:
@@ -92,118 +225,11 @@ class SwaggerConverter:
                     path = "/" + path
                 swagger_resource = swagger["paths"][path] = OrderedDict()
                 self.logger.debug("  Processing resource for %s", path)
-                # Resource level parameters
-                try:
-                    # wadllib can't get parameters w/out media types (e.g. path
-                    # params?)
-                    try:
-                        params = resource.parameters("application/json")
-                    except (UnsupportedMediaTypeError, AttributeError):
-                        self.logger.warning(
-                            "No support for application/json for resource at %s", path
-                        )
-                        params = []
-                    for param in resource.tag.findall(
-                        "./" + WADL.qname("wadl", "param")
-                    ):
-                        params.append(wadllib.application.Parameter(resource, param))
-                    for param in params:
-                        swagger_param = self.build_param(param)
-                        if swagger_param is not None:
-                            if "parameters" not in swagger_resource:
-                                swagger_resource["parameters"] = []
-                            swagger_resource["parameters"].append(swagger_param)
-                except AttributeError:
-                    self.logger.debug(
-                        "   WARN: wadllib can't get parameters, possibly a wadllib bug"
-                    )
-                    self.logger.debug(
-                        "     (It seems like it only works if the resource has a GET method"
-                    )
+                # Process resource parameters
+                self._process_resource_parameters(resource, path, swagger_resource)
 
                 for method in resource.method_iter:
-                    self.logger.debug("    Processing method %s %s", method.name, path)
-                    verb = method.name
-                    if self.autofix and verb == "copy":
-                        self.logger.warning(
-                            "Autofix: Using PUT instead of COPY verb (OpenStack services accept either, Swagger does not allow COPY)"
-                        )
-                        verb = "put"
-                    swagger_method = swagger_resource[verb] = OrderedDict()
-                    # Rackspace specific...
-                    if "{http://docs.rackspace.com/api}id" in method.tag.attrib:
-                        swagger_method["operationId"] = method.tag.attrib[
-                            "{http://docs.rackspace.com/api}id"
-                        ]
-                    swagger_method["summary"] = self.build_summary(method, path)
-                    description = DocHelper.short_desc_as_markdown(method)
-                    if description is not None:
-                        swagger_method["description"] = folded(description)
-                    # swagger_method['operationId'] = method.tag.attrib['id']
-                    # swagger_method['consumes'] = []
-                    swagger_method["produces"] = []
-                    swagger_method["responses"] = OrderedDict()
-                    if method.request.tag is not None:
-                        request = method.request
-                        for representation in request.representations:
-                            # Swagger schema needs to be updated to allow consumes here
-                            # swagger_method['consumes'].append(representation.media_type)
-                            for param in representation.params(resource):
-                                swagger_param = self.build_param(param)
-                                if swagger_param is not None:
-                                    if "parameters" not in swagger_method:
-                                        swagger_method["parameters"] = []
-                                    swagger_method["parameters"].append(swagger_param)
-
-                    if method.response.tag is not None:
-                        response = method.response
-                        # Not properly iterable - plus we're focused on json
-                        # for representation in response.representation:
-                        representation = response.get_representation_definition(
-                            "application/json"
-                        )
-                        if representation is not None:
-                            swagger_method["produces"].append(representation.media_type)
-                        try:
-                            statuses = response.tag.attrib["status"].split()
-                        except KeyError as e:
-                            raise BadWADLError(
-                                "Response has no status", e, wadl_file
-                            ) from e
-
-                        for status in statuses:
-                            swagger_method["responses"][int(status)] = (
-                                self.build_response(response)
-                            )
-
-                            code_sample = None
-                            code_samples = response.tag.findall(
-                                ".//"
-                                + WADL.qname("docbook", "programlisting")
-                                + '[@language="javascript"]'
-                            )
-                            if code_samples:
-                                try:
-                                    # if there is more than one, the first is
-                                    # usually HTTP headers
-                                    code_sample = code_samples[-1].text
-                                    code_sample = self.fix_json(code_sample)
-                                except ValueError as e:
-                                    error = WADLParseError(
-                                        "Unparsable code sample",
-                                        wadl_file,
-                                        swagger_method["summary"],
-                                        e,
-                                    )
-                                    if self.strict:
-                                        raise error
-                                    else:
-                                        self.logger.error(str(error))
-
-                            if code_sample:
-                                swagger_method["responses"][int(status)]["examples"] = (
-                                    self.build_code_sample(code_sample)
-                                )
+                    self._process_method(method, resource, path, swagger_resource)
             swagger = merge_dicts(swagger, defaults)
             return swagger
         except WADLError as e:
@@ -346,8 +372,11 @@ class SwaggerConverter:
                     param["description"] = folded(description)
         return param
 
-    def build_response(self, wadl_response):
-        status = wadl_response.tag.attrib["status"]
+    def format_id(self, id):
+        return re.sub(r"([a-z])([A-Z])", r"\1 \2", id)
+
+    def build_response(self, wadl_response, status):
+        # status is passed to avoid KeyError and allow autofix
         doc_tag = DocHelper.doc_tag(wadl_response)
         if doc_tag is not None and doc_tag.text is not None:
             description = " ".join(doc_tag.text.split())
